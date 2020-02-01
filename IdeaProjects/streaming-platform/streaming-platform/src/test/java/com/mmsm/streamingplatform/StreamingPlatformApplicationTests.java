c592428fa24851f3bdd55fa064d0ac24a483ad4f
marszm <mariusz.szmer@gmail.com>
2020-02-01 19:50:48 +0100

    Initial commit

---
+++ IdeaProjects/streaming-platform/streaming-platform/src/test/java/com/mmsm/streamingplatform/StreamingPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mmsm.streamingplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamingPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
