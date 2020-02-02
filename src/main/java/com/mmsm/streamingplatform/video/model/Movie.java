e217924ca419f54e7361d214a3d8d0946863be3a
Marcin Machalica <marcin.h.machalica@gmail.com>
2020-02-02 18:39:04 +0100

    Hotfix

--- src/main/java/com/mmsm/streamingplatform/video/model/Movie.java
+++ src/main/java/com/mmsm/streamingplatform/video/model/Movie.java
@@ -2,9 +2,14 @@ package com.mmsm.streamingplatform.video.model;
 
 import lombok.Data;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
 @Data
+@Entity
 public class Movie {
 
+    @Id
     private  Long id;
     private String path;
 
