edc76d9fe63f5eadc7dfa902ca65d2ceb9c21efa
marszm <mariusz.szmer@gmail.com>
2020-02-03 21:39:39 +0100

    code review

--- server/streamingplatform/src/main/java/com/mmsm/streamingplatform/video/model/VideoDto.java
+++ server/streamingplatform/src/main/java/com/mmsm/streamingplatform/video/model/VideoDto.java
@@ -1,5 +1,6 @@
 package com.mmsm.streamingplatform.video.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -7,6 +8,8 @@ import lombok.Getter;
 @Getter
 public class VideoDto {
 
+    @JsonIgnoreProperties()
     private Long id;
+    @JsonIgnoreProperties
     private String path;
 }
